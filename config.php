d9434c3850581af2179e830eefab0f5ff9d8f994
Brandon Allen <ballen@awesomemotive.com>
2025-12-15 18:51:40 -0600

    Simplify config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -6,33 +6,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 //Prevent directly browsing to the file
-if (function_exists('plugin_dir_url')) 
-{
+if ( function_exists( 'plugin_dir_url' ) ) {
 
-	$host = $_SERVER['HTTP_HOST'];
+	define( 'BEACONBY_VERSION', '1.5.8' );
+	define( 'BEACONBY_HOMEPAGE', 'https://beacon.by/' );
+	define( 'BEACONBY_HELPLINK', 'https://beacon.by/wordpress' );
+	define( 'BEACONBY_PLUGIN_URL', plugin_dir_url(__FILE__) );
+	define( 'BEACONBY_PLUGIN_PATH', plugin_dir_path(__FILE__) );
+	define( 'BEACONBY_SITE_URL', get_site_url() );
+	define( 'BEACONBY_INCLUDE_TITLES', true );
+	define( 'BEACONBY_PER_PAGE', 50 );
 
-	switch ($host)
-	{
-	case 'wordpress':
-		$create_target = 'http://localhost';
-		break;
-
-	default:
-		$create_target = 'https://beacon.by';
-		break;
+	if ( ! defined( 'BEACONBY_CREATE_TARGET' ) ) {
+		define('BEACONBY_CREATE_TARGET', 'https://beacon.by' );
 	}
-
-	define('BEACONBY_VERSION',      '1.5.8');
-	define("BEACONBY_HOMEPAGE",     "https://beacon.by/");
-	define("BEACONBY_HELPLINK",     "https://beacon.by/wordpress");
-	define('BEACONBY_PLUGIN_URL',   plugin_dir_url(__FILE__));
-	define('BEACONBY_PLUGIN_PATH',  plugin_dir_path(__FILE__));
-	define('BEACONBY_SITE_URL', get_site_url());
-	define('BEACONBY_INCLUDE_TITLES', true);
-	define('BEACONBY_PER_PAGE', 50);
-
-	define('BEACONBY_CREATE_TARGET',    $create_target);
-
 }
 
 
